8bb377ecf87214be7de1de4fae5303a24a8d28e8
DanilaAneichik <danila.aneychik2005@gmail.com>
2022-10-16 00:51:29 +0300

    Upgrade player movement.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,28 +1,42 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-   #region Variables
+    #region Variables
 
-   [SerializeField] private float _speed = 3f; 
-   
+    [Header("Base Settings")]
+    [SerializeField] private float _speed = 3f;
+    [SerializeField] private CharacterController _characterController;
+    [SerializeField] private float _gravityMultiplier = 1f; 
 
-   #endregion
-   #region Unity lifecycle
+    [Header("Grounded")]
+    [SerializeField] private Transform _checkGroundTransform;
+    [SerializeField] private float _checkGroundedRadius;
+    [SerializeField] private LayerMask _checkGroundMask; 
+    #endregion
 
-   private void Update()
-   {
-      float horizontal = Input.GetAxis("Horizontal");
-      float vertical = Input.GetAxis("Vertical");
 
-      Vector3 moveVector = transform.right * horizontal + transform.forward * vertical;
-      Vector3 position = transform.position;
-      position += moveVector * _speed * Time.deltaTime;
-      transform.position = position;  
-   }
+    #region Unity lifecycle
 
-   #endregion
+    private void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 moveVector = transform.right * horizontal + transform.forward * vertical;
+        moveVector *= (_speed * Time.deltaTime);
+
+        _characterController.Move(moveVector);
+
+
+        bool isGrounded = Physics.CheckSphere(_checkGroundTransform.position, _checkGroundedRadius, _checkGroundMask);
+        Debug.Log($"IsGrounded {isGrounded}");
+        
+        Vector3 fallVector = Vector3.zero;
+        float gravity = Physics.gravity.y * _gravityMultiplier;
+        fallVector.y += gravity * Time.deltaTime;
+        _characterController.Move(fallVector);
+    }
+
+    #endregion
 }
