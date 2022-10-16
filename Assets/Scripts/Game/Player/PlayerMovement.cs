980144c5db00f977e5b7410dbd4aa5fae29421e7
DanilaAneichik <danila.aneychik2005@gmail.com>
2022-10-16 15:54:34 +0300

    Add naughty attributes.

---
+++ Assets/Scripts/Game/Player/PlayerMovement.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+
+namespace Platformer3D.Game
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        #region Variables
+
+        [Header("Base Settings")]
+        [SerializeField] private float _speed = 3f;
+        [SerializeField] private CharacterController _characterController;
+        [SerializeField] private float _gravityMultiplier = 1f;
+
+        [Header("Grounded")]
+        [SerializeField] private Transform _checkGroundTransform;
+        [SerializeField] private float _checkGroundedRadius;
+        [SerializeField] private LayerMask _checkGroundMask;
+
+        [Header("Jump")]
+        [SerializeField] private float _jumpHeight = 2f;
+
+        private Vector3 _fallVector;
+        private Transform _cachedTransform; 
+
+        #endregion
+
+
+        #region Unity lifecycle
+
+        private void Awake()
+        {
+            _cachedTransform = transform; 
+        }
+
+        private void Update()
+        {
+            float horizontal = Input.GetAxis("Horizontal");
+            float vertical = Input.GetAxis("Vertical");
+
+            Vector3 moveVector = _cachedTransform.right * horizontal + _cachedTransform.forward * vertical;
+            moveVector *= (_speed * Time.deltaTime);
+
+            _characterController.Move(moveVector);
+
+
+            bool isGrounded = Physics.CheckSphere(_checkGroundTransform.position, _checkGroundedRadius, _checkGroundMask);
+            Debug.Log($"IsGrounded {isGrounded}");
+
+            if (isGrounded && _fallVector.y < 0)
+            {
+                _fallVector.y = 0;
+            }
+        
+            float gravity = Physics.gravity.y * _gravityMultiplier;
+        
+            if (isGrounded && Input.GetButtonDown("Jump"))
+            {
+                _fallVector.y = Mathf.Sqrt(_jumpHeight * -2f * gravity);
+            }
+
+            _fallVector.y += gravity * Time.deltaTime;
+            _characterController.Move(_fallVector * Time.deltaTime);
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/Scripts/Game/Player/PlayerRotator.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Platformer3D.Game
+{
+    public class PlayerRotator : MonoBehaviour
+    {
+        #region Variables
+
+        [SerializeField] private float _speed = 3f;
+
+        private Vector3 _previousMousePosition; 
+
+        #endregion
+
+
+        #region Unity lifecycle
+
+        private void Start()
+        {
+            _previousMousePosition = Input.mousePosition;
+        }
+
+        private void Update()
+        {
+            Vector3 mousePosition = Input.mousePosition;
+            Vector3 delta = _previousMousePosition - mousePosition;
+            float rotationDelta = delta.x; 
+            transform.Rotate(transform.up, rotationDelta * _speed * Time.deltaTime);
+            _previousMousePosition = mousePosition; 
+        }
+
+        #endregion
+    }
+}
