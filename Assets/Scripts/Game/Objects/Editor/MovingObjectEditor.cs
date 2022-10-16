ed57855f6afed2783425f8cd6514544fbc7bf546
DanilaAneichik <danila.aneychik2005@gmail.com>
2022-10-16 23:42:26 +0300

    Add moving platform logic and gizmos for moving platform.

---
+++ Assets/Scripts/Game/Objects/Editor/MovingObjectEditor.cs
@@ -0,0 +1,62 @@
+﻿using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+
+namespace Platformer3D.Game.Objects
+{
+    [CustomEditor(typeof(MovingObject))]
+    public class MovingObjectEditor : Editor
+    {
+        [DrawGizmo(GizmoType.Selected | GizmoType.NonSelected)]
+        private static void Draw(MovingObject movingObject, GizmoType gizmoType)
+        {
+            if (!ShouldDraw(movingObject, gizmoType))
+                return;
+
+
+            if (movingObject.FromTransform == null || movingObject.ToTransform == null)
+            {
+                return;
+            }
+
+            Gizmos.color = Color.blue;
+            var fromTransformPosition = movingObject.FromTransform.position;
+            var toTransformPosition = movingObject.ToTransform.position;
+            Gizmos.DrawSphere(fromTransformPosition, 0.1f);
+            Gizmos.DrawSphere(toTransformPosition, 0.1f);
+
+            Gizmos.color = Color.green;
+            Gizmos.DrawLine(fromTransformPosition, toTransformPosition);
+        }
+
+        private static bool ShouldDraw(MovingObject movingObject, GizmoType gizmoType)
+        {
+            if (gizmoType == GizmoType.Selected)
+                return true;
+
+            Transform parent = movingObject.transform.parent;
+            if (parent == Selection.activeTransform)
+                return true;
+            
+
+            for (int i = 0; i < parent.childCount; i++)
+            {
+                if (parent.GetChild(i) == Selection.activeTransform)
+                    return true;
+            }
+
+            return false; 
+        }
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            MovingObject movingObject = (MovingObject) target; 
+            if (GUILayout.Button($"Test Button"))
+            {
+                movingObject.Play();
+            }
+                
+        }
+    }
+}
---
+++ Assets/Scripts/Game/Objects/MovingObject.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using DG.Tweening;
+using UnityEngine;
+
+namespace Platformer3D.Game
+{
+    public class MovingObject : MonoBehaviour
+    {
+        #region Variables
+
+        [SerializeField] private Transform _fromTransform;
+        [SerializeField] private Transform _toTransform;
+        [SerializeField] private float _duration = 1f;
+        [SerializeField] private float _delayInFromPosition = 1f; 
+        [SerializeField] private float _delayInToPosition = 1f; 
+        [SerializeField] private Ease _ease;
+
+        private Tween _tween;
+        public Transform FromTransform => _fromTransform;
+
+        public Transform ToTransform => _toTransform;
+
+        #endregion
+
+
+        #region Unity lifecycle
+
+        private void Awake()
+        {
+            transform.position = _fromTransform.position; 
+        }
+
+        private void Start()
+        {
+            Play();
+        }
+
+        #endregion
+        #region Public methods
+
+        public void Play()
+        {
+            _tween?.Kill();
+
+            Sequence sequence = DOTween.Sequence();
+            sequence.AppendInterval(_delayInFromPosition); 
+            sequence.Append(_tween = transform
+                .DOMove(_toTransform.position, _duration)
+                .SetEase(_ease));
+            sequence.AppendInterval(_delayInToPosition);
+            sequence.Append(transform
+                .DOMove(_fromTransform.position, _duration)
+                .SetEase(_ease));
+            sequence.SetLoops(-1);
+
+            _tween = sequence; 
+        }
+
+        #endregion
+    }
+}
